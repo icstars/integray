e42e1a00e388c2919b220d3c2245d4272b3f5a53
Dami Oye <doyeyipo@icstars.org>
2017-01-25 18:24:37 +0000

    dami: location others

--- backend/routes.js
+++ backend/routes.js
@@ -3,6 +3,9 @@
 var router = require('express').Router();
 var database = require('./c37_modules/database');
 
+var multer = require ('multer');
+var upload = multer({ dest: 'frontend/html/uploads/'});
+
 module.exports = function() {
 
   router.get('/', function(req,res){
@@ -16,15 +19,29 @@ module.exports = function() {
  router.get('/connects', function(req,res){
     return res.render('connects.html');
   });
+  
+  router.get('/challenge', function(req,res){
+    return res.sender('ant.html');
+  });
+  
+  router.get('/editchallenge', function(req,res){
+    return res.sender('editchallenge.html');
+  });
+  router.get('/connects#Gallery', function (req,res){
+    return res.sender('connects.html#imagesData');
+  });
+  
+   router.get('/uploads', function(req,res){
+    return res.render('land.html');
+    });
 
-  router.get('/trains/data', function(req,res){
+  router.get('/trains/datas', function(req,res){
     database.executeQuery("SELECT * FROM trains", function(results) {
       res.send(results);
     });
   });
   
-  
-   router.post('/trains/data', function(req,res){
+     router.post('/trains/data', function(req,res){
     database.executeQuery("insert into trains(trainNumber, linecolor, inservice)", function(results) {
       res.send(results);
     });
@@ -35,11 +52,15 @@ module.exports = function() {
       res.send(results);
     });
     });
-  router.post('/uploads.php', function(req,res){
-    database.executeQuery("insert into contest(contest, linecolor, inservice)", function(results) {
-      res.send(results);
+  router.post('/uploads/images', upload.single('Upload') , function(req,res,next){
+    var filename= req.file.filename;
+    console.log ("Oi User");
+    console.log ("Your image(s) got uploaded");
+    return res.send("<html><img src='/uploads/" + filename + "'/></html>");
     });
-    });
+  router.post('/connects', function (req,res){
+    return res.render('connects.html');
+  }); 
 
   /* Your code here */
   /*router.post('/trains/data', function(req, res){
